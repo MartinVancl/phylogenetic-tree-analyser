d8c074355b5a87d8e058c4d62e1304180431ed62
Martin Vancl <martin@vancl.me>
2021-03-07 21:49:13 +0100

    Progress in OOPing previous version

--- pta.py
+++ pta.py
@@ -4,12 +4,80 @@ from sys import argv
 from os import path, listdir
 from os.path import isfile, join
 
-def main():
-    args = argv[1:]
-    print("args=", end='')
-    print(args)
-    argsLen = len(args)
+class argParser:
+    def __init__(self, verbose=False):
+        self.fullArgsString = argv[0]
+        self.args = argv[1:]
+        self.argsLength = len(self.args)
 
+        if verbose:
+            print("args=", end='')
+            print(self.args)
+
+    def isFlagSet(self, flag):
+        return f"-{flag}" in self.args
+
+    def getFlagPosition(self, flag):
+        if self.isFlagSet(flag):
+            return self.args.index(f"-{flag}")
+        else:
+            return None
+
+    def getFlagValue(self, flag):
+        fv = self.isFlagValued(flag, True)
+        if fv[0]:
+            return self.args[fv[1]]
+        else:
+            return None
+            
+    def isFlagValued(self, flag, giveIndex=False):
+        i = self.getFlagPosition(flag)
+        if type(i) == int:
+            if i + 1 < self.argsLength and self.args[i + 1][0] != '-':
+                # there is an argument after the flag and it is not another flag
+                return (True, i + 1) if giveIndex else True
+            else:
+                # flag is not valued with argument
+                return (False, None) if giveIndex else False
+        else:
+            #flag is not set
+            return (False, None) if giveIndex else False
+
+class treeFilesManager:
+    def __init__(self, args):
+        self.args = args
+        if self.args.isFlagValued('d'):
+            # working directory is set by -d ...
+            wd = self.args.getFlagValue('d')
+            if path.exists(wd):
+                self.wd = wd
+            else:
+                exit(f"Working directory '{wd}' set by -d does not exist.")
+        else:
+            self.wd = '.'
+
+    def fileExists(self, filename):
+        return isfile(join(self.wd, filename))
+
+    def loadFromFile(self, filename):
+        pass
+    def loadFromDirectory(self, directory):
+        pass
+    def filterExisting(self):
+        pass
+
+def main():
+
+    args = argParser()
+
+    treeFiles = treeFilesManager(args)
+
+    print("exists", treeFiles.fileExists("Takayama-helix-ANV-100120_homologs.fasta.tre"))
+
+
+
+def noexec():
+    # tis an o'code that i dissect and transform into something more OOP
 
     # check input file argument -f
     treesFile = False
